4c33b830519f165c96a056234ad302137af049be
vsl700 <51147745+vsl700@users.noreply.github.com>
2021-09-10 18:25:56 +0300

    Added a Regressor of kNN (SVM doesn't work as Regressor)
    kNN is a bit better in its Regressor form, but not as good as SVM (RMSE: 0.53 at best)

--- exercising/wine_dataset_solution_knn_regressor.py
+++ exercising/wine_dataset_solution_knn_regressor.py
@@ -1,15 +1,14 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import BaggingClassifier
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import BaggingRegressor
 from sklearn import metrics
 from math import sqrt
 
 
 def print_scores(y_true, y_pr):
-    print('RMSE (train):', sqrt(metrics.mean_squared_error(y_true, y_pr)))
-    print('Accuracy:', metrics.accuracy_score(y_true, y_pr))
+    print('RMSE:', sqrt(metrics.mean_squared_error(y_true, y_pr)))
 
 
 data = datasets.load_wine()
@@ -17,39 +16,39 @@ print(len(data.data[:, 0]))  # 178
 
 X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.35, random_state=123)
 
-clf = KNeighborsClassifier(n_neighbors=3)  # train error is 0 with distance weights
+clf = KNeighborsRegressor(n_neighbors=3)  # train error is 0 with distance weights
 clf.fit(X_train, y_train)
 
 y_pred = clf.predict(X_test)
-print_scores(y_test, y_pred)  # RMSE: 0.7559289460184544, Acc: 0.6666666666666666
+print_scores(y_test, y_pred)  # RMSE: 0.5968760532369259
 
 y_pred = clf.predict(X_train)
-print_scores(y_train, y_pred)  # RMSE: 0.5021692075819447, Acc: 0.8521739130434782
+print_scores(y_train, y_pred)  # RMSE: 0.3768673314407159
 print('\n')
 
 # Improving kNN
 parameters = {"n_neighbors": range(1, 50),
               "weights": ["uniform", "distance"]}
-grid_search = GridSearchCV(KNeighborsClassifier(), parameters)
+grid_search = GridSearchCV(KNeighborsRegressor(), parameters)
 grid_search.fit(X_train, y_train)
 print(grid_search.best_params_, '\n\n')
 
 y_pred = grid_search.predict(X_test)
-print_scores(y_test, y_pred)  # RMSE: 0.6546536707079771, Acc: 0.7142857142857143
+print_scores(y_test, y_pred)  # RMSE: 0.531750607527887
 
 y_pred = grid_search.predict(X_train)
-print_scores(y_train, y_pred)  # RMSE: 0.0, Acc: 1.0
+print_scores(y_train, y_pred)  # RMSE: 0.0
 print('\n')
 
 
 best_k = grid_search.best_params_["n_neighbors"]
 best_weights = grid_search.best_params_["weights"]
-bagged_knn = KNeighborsClassifier(n_neighbors=best_k, weights=best_weights)
+bagged_knn = KNeighborsRegressor(n_neighbors=best_k, weights=best_weights)
 
-bagging_model = BaggingClassifier(bagged_knn, n_estimators=100)
+bagging_model = BaggingRegressor(bagged_knn, n_estimators=100)
 bagging_model.fit(X_train, y_train)
 y_pred = bagging_model.predict(X_test)
-print_scores(y_test, y_pred)  # RMSE: 0.6546536707079771, Acc: 0.7142857142857143
+print_scores(y_test, y_pred)  # RMSE: 0.5296233305284472
 
 y_pred = bagging_model.predict(X_train)
-print_scores(y_train, y_pred)  # RMSE: 0.0, Acc: 1.0
+print_scores(y_train, y_pred)  # RMSE: 0.17819336310128217
