59c20fc10c270c63393bd7c6fbe59b04d9449b34
vsl700 <51147745+vsl700@users.noreply.github.com>
2021-10-28 00:18:29 +0300

    Creating the model

--- convolutional_neural_networks/cnn_fashion_mnist.py
+++ convolutional_neural_networks/cnn_fashion_mnist.py
@@ -4,6 +4,14 @@ from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
+import keras
+from keras.models import Sequential, Input, Model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers.advanced_activations import LeakyReLU
+
+
 (train_X, train_Y), (test_X, test_Y) = fashion_mnist.load_data()
 
 print('Training data shape:', train_X.shape, train_Y.shape)
@@ -52,3 +60,31 @@ print('After conversion to one-hot:', train_Y_one_hot[0])
 # Split training data into Training and Validation data
 train_X, valid_X, train_label, valid_label = train_test_split(train_X, train_Y_one_hot, test_size=0.2, random_state=13)
 print(f'\n{train_X.shape}, {valid_X.shape}, {train_label.shape}, {valid_label.shape}')
+
+
+#
+#
+# Modeling the data
+batch_size = 64
+epochs = 20
+num_classes = 10
+
+fashion_model = Sequential()
+fashion_model.add(Conv2D(32, kernel_size=(3, 3), activation='linear', input_shape=(28, 28, 1), padding='same'))
+fashion_model.add(LeakyReLU(alpha=0.1))
+fashion_model.add(MaxPooling2D((2, 2), padding='same'))
+fashion_model.add(Conv2D(64, (3, 3), activation='linear', padding='same'))
+fashion_model.add(LeakyReLU(alpha=0.1))
+fashion_model.add(MaxPooling2D((2, 2), padding='same'))
+fashion_model.add(Conv2D(128, (3, 3), activation='linear', padding='same'))
+fashion_model.add(LeakyReLU(alpha=0.1))
+fashion_model.add(MaxPooling2D((2, 2), padding='same'))
+fashion_model.add(Flatten())
+fashion_model.add(Dense(128, activation='linear'))
+fashion_model.add(LeakyReLU(alpha=0.1))
+fashion_model.add(Dense(num_classes, activation='linear'))
+
+fashion_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),
+                      metrics=['accuracy'])
+
+fashion_model.summary()
