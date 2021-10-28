b3533e6207101aa9d1ccbd27cf52a7ec004729ff
vsl700 <51147745+vsl700@users.noreply.github.com>
2021-10-29 00:24:35 +0300

    Created the model (with reduced overfitting)

--- convolutional_neural_networks/cnn_fashion_mnist.py
+++ convolutional_neural_networks/cnn_fashion_mnist.py
@@ -6,11 +6,11 @@ import matplotlib.pyplot as plt
 
 import keras
 from keras.models import Sequential, Input, Model
+from keras.models import load_model
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers.normalization import BatchNormalization
 from keras.layers.advanced_activations import LeakyReLU
-
 
 (train_X, train_Y), (test_X, test_Y) = fashion_mnist.load_data()
 
@@ -22,7 +22,6 @@ classes = np.unique(train_Y)
 nClasses = len(classes)
 print('Total number of outputs:', 10)
 print('Output classes:', classes)
-
 
 plt.figure(figsize=[5, 5])
 
@@ -50,7 +49,6 @@ test_X = test_X.astype('float32')
 train_X = train_X / 255
 test_X = test_X / 255
 
-
 # Convert to one-hot encoding vector (for categorizing by the network)
 train_Y_one_hot = to_categorical(train_Y)
 test_Y_one_hot = to_categorical(test_Y)
@@ -60,7 +58,6 @@ print('After conversion to one-hot:', train_Y_one_hot[0])
 # Split training data into Training and Validation data
 train_X, valid_X, train_label, valid_label = train_test_split(train_X, train_Y_one_hot, test_size=0.2, random_state=13)
 print(f'\n{train_X.shape}, {valid_X.shape}, {train_label.shape}, {valid_label.shape}')
-
 
 #
 #
@@ -73,18 +70,50 @@ fashion_model = Sequential()
 fashion_model.add(Conv2D(32, kernel_size=(3, 3), activation='linear', input_shape=(28, 28, 1), padding='same'))
 fashion_model.add(LeakyReLU(alpha=0.1))
 fashion_model.add(MaxPooling2D((2, 2), padding='same'))
+fashion_model.add(Dropout(0.25))
 fashion_model.add(Conv2D(64, (3, 3), activation='linear', padding='same'))
 fashion_model.add(LeakyReLU(alpha=0.1))
 fashion_model.add(MaxPooling2D((2, 2), padding='same'))
+fashion_model.add(Dropout(0.25))
 fashion_model.add(Conv2D(128, (3, 3), activation='linear', padding='same'))
 fashion_model.add(LeakyReLU(alpha=0.1))
 fashion_model.add(MaxPooling2D((2, 2), padding='same'))
+fashion_model.add(Dropout(0.25))
 fashion_model.add(Flatten())
 fashion_model.add(Dense(128, activation='linear'))
 fashion_model.add(LeakyReLU(alpha=0.1))
-fashion_model.add(Dense(num_classes, activation='linear'))
+fashion_model.add(Dropout(0.25))
+fashion_model.add(Dense(num_classes, activation='softmax'))
 
 fashion_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),
                       metrics=['accuracy'])
 
-fashion_model.summary()
+fashion_model.summary()  # Shows info about layers and parameters
+
+fashion_train = fashion_model.fit(train_X, train_label, batch_size=batch_size, epochs=epochs, verbose=1,
+                                  validation_data=(valid_X, valid_label))
+
+# Save the trained model
+fashion_model.save("fashion_model_dropout.h5py")
+
+test_eval = fashion_model.evaluate(test_X, test_Y_one_hot, verbose=1)
+print('Test loss:', test_eval[0])
+print('Test accuracy:', test_eval[1])
+
+
+accuracy = fashion_train.history['accuracy']
+val_accuracy = fashion_train.history['val_accuracy']
+loss = fashion_train.history['loss']
+val_loss = fashion_train.history['val_loss']
+epochs = range(len(accuracy))
+plt.figure(clear=True)
+plt.plot(epochs, accuracy, 'bo', label='Training accuracy')
+plt.plot(epochs, val_accuracy, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
