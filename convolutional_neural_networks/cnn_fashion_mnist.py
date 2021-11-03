610450845b0706cfd5774a56bf307fb24e136c84
vsl700 <51147745+vsl700@users.noreply.github.com>
2021-11-03 17:39:52 +0200

    Made the save/load model implementation

--- convolutional_neural_networks/cnn_fashion_mnist.py
+++ convolutional_neural_networks/cnn_fashion_mnist.py
@@ -66,54 +66,59 @@ batch_size = 64
 epochs = 20
 num_classes = 10
 
-fashion_model = Sequential()
-fashion_model.add(Conv2D(32, kernel_size=(3, 3), activation='linear', input_shape=(28, 28, 1), padding='same'))
-fashion_model.add(LeakyReLU(alpha=0.1))
-fashion_model.add(MaxPooling2D((2, 2), padding='same'))
-fashion_model.add(Dropout(0.25))
-fashion_model.add(Conv2D(64, (3, 3), activation='linear', padding='same'))
-fashion_model.add(LeakyReLU(alpha=0.1))
-fashion_model.add(MaxPooling2D((2, 2), padding='same'))
-fashion_model.add(Dropout(0.25))
-fashion_model.add(Conv2D(128, (3, 3), activation='linear', padding='same'))
-fashion_model.add(LeakyReLU(alpha=0.1))
-fashion_model.add(MaxPooling2D((2, 2), padding='same'))
-fashion_model.add(Dropout(0.25))
-fashion_model.add(Flatten())
-fashion_model.add(Dense(128, activation='linear'))
-fashion_model.add(LeakyReLU(alpha=0.1))
-fashion_model.add(Dropout(0.25))
-fashion_model.add(Dense(num_classes, activation='softmax'))
+try:
+    fashion_model = load_model("fashion_model_dropout.h5py")
+    fashion_model.summary()
+except IOError:
+    fashion_model = Sequential()
+    fashion_model.add(Conv2D(32, kernel_size=(3, 3), activation='linear', input_shape=(28, 28, 1), padding='same'))
+    fashion_model.add(LeakyReLU(alpha=0.1))
+    fashion_model.add(MaxPooling2D((2, 2), padding='same'))
+    fashion_model.add(Dropout(0.25))
+    fashion_model.add(Conv2D(64, (3, 3), activation='linear', padding='same'))
+    fashion_model.add(LeakyReLU(alpha=0.1))
+    fashion_model.add(MaxPooling2D((2, 2), padding='same'))
+    fashion_model.add(Dropout(0.25))
+    fashion_model.add(Conv2D(128, (3, 3), activation='linear', padding='same'))
+    fashion_model.add(LeakyReLU(alpha=0.1))
+    fashion_model.add(MaxPooling2D((2, 2), padding='same'))
+    fashion_model.add(Dropout(0.25))
+    fashion_model.add(Flatten())
+    fashion_model.add(Dense(128, activation='linear'))
+    fashion_model.add(LeakyReLU(alpha=0.1))
+    fashion_model.add(Dropout(0.25))
+    fashion_model.add(Dense(num_classes, activation='softmax'))
 
-fashion_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),
-                      metrics=['accuracy'])
+    fashion_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),
+                          metrics=['accuracy'])
 
-fashion_model.summary()  # Shows info about layers and parameters
+    fashion_model.summary()  # Shows info about layers and parameters
 
-fashion_train = fashion_model.fit(train_X, train_label, batch_size=batch_size, epochs=epochs, verbose=1,
-                                  validation_data=(valid_X, valid_label))
+    fashion_train = fashion_model.fit(train_X, train_label, batch_size=batch_size, epochs=epochs, verbose=1,
+                                      validation_data=(valid_X, valid_label))
 
-# Save the trained model
-fashion_model.save("fashion_model_dropout.h5py")
+    # Save the trained model
+    fashion_model.save("fashion_model_dropout.h5py")
+
+    accuracy = fashion_train.history['accuracy']
+    val_accuracy = fashion_train.history['val_accuracy']
+    loss = fashion_train.history['loss']
+    val_loss = fashion_train.history['val_loss']
+    epochs = range(len(accuracy))
+    plt.figure(clear=True)
+    plt.plot(epochs, accuracy, 'bo', label='Training accuracy')
+    plt.plot(epochs, val_accuracy, 'b', label='Validation accuracy')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.figure()
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    plt.show()
 
 test_eval = fashion_model.evaluate(test_X, test_Y_one_hot, verbose=1)
 print('Test loss:', test_eval[0])
 print('Test accuracy:', test_eval[1])
 
 
-accuracy = fashion_train.history['accuracy']
-val_accuracy = fashion_train.history['val_accuracy']
-loss = fashion_train.history['loss']
-val_loss = fashion_train.history['val_loss']
-epochs = range(len(accuracy))
-plt.figure(clear=True)
-plt.plot(epochs, accuracy, 'bo', label='Training accuracy')
-plt.plot(epochs, val_accuracy, 'b', label='Validation accuracy')
-plt.title('Training and validation accuracy')
-plt.legend()
-plt.figure()
-plt.plot(epochs, loss, 'bo', label='Training loss')
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
-plt.title('Training and validation loss')
-plt.legend()
-plt.show()
